7e7f95f5b3f5855b4fb110ba26e97cad744e638c
Nick Nance <nance.nick@gmail.com>
2019-02-09 17:24:54 -0800

    web page for linking

--- src/plaidClient.ts
+++ src/plaidClient.ts
@@ -1,12 +1,14 @@
 import * as plaid from "plaid";
 
-const env = process.env.PLAID_ENV === "development" ? plaid.environments.development :
-            process.env.PLAID_ENV === "production" ? plaid.environments.production :
+export const env = process.env.PLAID_ENV;
+
+export const plaidEnv = env === "development" ? plaid.environments.development :
+            env === "production" ? plaid.environments.production :
             plaid.environments.sandbox;
 
 export const client = new plaid.Client(
     process.env.PLAID_CLIENT_ID || "",
     process.env.PLAID_SECRET || "",
     process.env.PLAID_PUBLIC_KEY || "",
-    env,
+    plaidEnv,
 );
---
+++ src/server.ts
@@ -0,0 +1,51 @@
+import bodyParser from "body-parser";
+import express from "express";
+
+import { getTransactionsFor30Days } from "./index";
+import { client, env } from "./plaidClient";
+
+// We store the access_token in memory - in production, store it in a secure
+// persistent data store
+let ACCESS_TOKEN = null;
+let PUBLIC_TOKEN = null;
+let ITEM_ID = null;
+
+// Accept the public_token sent from Link
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, resp) => resp.render("../pages/index", {
+  env,
+  publicKey: process.env.PLAID_PUBLIC_KEY,
+}));
+
+app.post("/get_access_token", (request, response, next) => {
+  PUBLIC_TOKEN = request.body.public_token;
+  // tslint:disable-next-line:no-console
+  console.dir(request.body, {depth: null});
+  // tslint:disable-next-line:no-console
+  console.log(`public_token: ${PUBLIC_TOKEN}`);
+  client.exchangePublicToken(PUBLIC_TOKEN, async (error, tokenResponse) => {
+    if (error != null) {
+      const msg = "Could not exchange public_token!";
+      // tslint:disable-next-line:no-console
+      console.log(`${msg}\n${JSON.stringify(error)}`);
+      return response.json({error: msg});
+    }
+    ACCESS_TOKEN = tokenResponse.access_token;
+    ITEM_ID = tokenResponse.item_id;
+    // tslint:disable-next-line:no-console
+    console.log("Access Token: " + ACCESS_TOKEN);
+    // tslint:disable-next-line:no-console
+    console.log("Item ID: " + ITEM_ID);
+    response.json({error: false});
+
+    const trans = await getTransactionsFor30Days(ACCESS_TOKEN);
+    // tslint:disable-next-line:no-console
+    console.dir(trans, {depth: null});
+  });
+});
+
+app.listen(8080);
